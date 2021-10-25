57cb545b313d23a5e80dd03e1670669dfed9a9d6
raphaRodrigo <r7r7p7@gmail.com>
2021-10-25 18:05:57 -0300

    Autoplay on modal open and pause on close.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,7 +1,10 @@
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
-  $("#trailerModal").on('hidden.bs.modal', function (e) {
-    $("#trailerModal iframe").attr("src", $("#trailerModal iframe").attr("src"));
-  });
+  $('#trailerModal').on('shown.bs.modal', function (event) {
+      $('#video-1')[0].play();
+    });
+    $('#trailerModal').on('hidden.bs.modal', function (event) {
+      $('#video-1')[0].pause();
+    });
 });
